2e9038f6c7a275ed5e19de63a10f7584b63d0219
Grace Wang <wangg2026@abbotsleigh.nsw.edu.au>
2026-03-23 18:09:32 +1100

    Set up initial routes and styling

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.get('/')
 def home():
-    return "Welcome"
+    return render_template('public/home.html')
+
+
+@app.get('/login')
+def login_form():
+    return render_template('public/login.html')
+
+
+@app.get('/register')
+def register_form():
+    return render_template('public/register.html')
+
+@app.get('/dashboard')
+def dashboard():
+    return render_template('app/dashboard.html')
+
+@app.get('/view-timetable')
+def timetable():
+    return render_template('app/timetable.html')
+
+@app.get('/add-event')
+def add_event_form():
+    return render_template('app/event-form.html')
+
+@app.get('/profile')
+def view_profile():
+    return render_template('app/profile.html')
+
+@app.get('/logout')
+def confirm_logout():
+    return render_template('public/logout.html')
 
 app.run(debug=True)
