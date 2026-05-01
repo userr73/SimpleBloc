e1da3b29c6b783d405b5b60a495f20db80143bdd
Grace Wang <wangg2026@abbotsleigh.nsw.edu.au>
2026-05-01 13:41:16 +1000

    Allow the addition of new datalist items to the database and add new events to the database

--- routes/app_routes.py
+++ routes/app_routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, request, session, abort
+from flask import render_template, request, session, abort, redirect, url_for
 
 from application import app
 from utils.validators import validate_event, validate_date
-from utils.events import select_week_dates
+from utils.events import select_week_dates, check_if_add_new_category, get_all_categories, get_category_id
+from utils.db import get_db
 
 def check_login():
     """Checks if a user id is set in the session"""
@@ -53,12 +54,14 @@ def timetable():
 @app.get('/add-event')
 def new_event_form():
     check_login()
-    return render_template('app/event-form.html')
+    categories_list = get_all_categories(session.get('user_id'))
+    return render_template('app/event-form.html', categories=categories_list)
 
 
 @app.post('/submit-new-event')
-def submit_new_event():
+def add_event():
     check_login()
+    
     # Dictionary for form data - used to repopulate the form
     form_data = {
         'event_date': request.form.get('event_date'),
@@ -74,11 +77,76 @@ def submit_new_event():
     errors = validate_event(form_data)
 
     if errors:
-        return render_template('app/event-form.html', errors=errors, data=form_data)
+        # Re-render the template with error messages and form data
+        return render_template('app/event-form.html', errors=errors, data=form_data, categories=get_all_categories(session.get('user_id')))
 
-    # If no errors, use temp submission form
-    form_data['event_date'] 
-    return render_template('app/temp-form-submit.html', data=form_data)
+    # Get the user id from the session
+    user_id = session.get('user_id')
+
+    # Get the user entered category name
+    category_name = form_data['category']
+    
+    if check_if_add_new_category(category_name, user_id):
+        # Add the category string into the database
+        conn = get_db()
+        cursor = conn.cursor()
+
+        sql = '''
+            INSERT INTO Categories
+                (user_id,
+                category_name)
+            VALUES (?, ?)
+        '''
+
+        data = (
+            session.get('user_id'),
+            category_name
+        )
+
+        cursor.execute(sql, data)
+
+        conn.commit()
+        conn.close()
+    
+    # Get the category id of the user inputted category name
+    category_id = get_category_id(user_id, category_name)
+
+    # Prepare database connection
+    conn = get_db()
+    cursor = conn.cursor()
+
+    # Insert the trip data into the database
+    sql = '''
+        INSERT INTO Events
+            (user_id,
+            event_date,
+            start_time,
+            end_time,
+            is_all_day_event,
+            event_title,
+            event_description,
+            category_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    '''
+    data = (
+        user_id,
+        form_data['event_date'],
+        form_data['start_time'],
+        form_data['end_time'],
+        form_data['is_all_day_event'],
+        form_data['event_title'],
+        form_data['event_description'],
+        category_id
+    )
+
+    cursor.execute(sql, data)
+
+    # Commit changes and close the database connection
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('dashboard'))
+
 
 
 @app.get('/edit-categories')
--- utils/events.py
+++ utils/events.py
@@ -1,5 +1,50 @@
 from datetime import date
 
+from utils.db import get_db, query_all, query_one
+
+def get_category_id(user_id, category_str):
+    """Finds the category id of a given category name string"""
+    sql = '''
+        SELECT category_id
+        FROM Categories
+        WHERE category_name = ? AND user_id = ?
+    '''
+
+    data = (category_str, user_id)
+
+    category_id = query_one(sql, data)
+
+    return dict(category_id)['category_id']
+
+
+
+def get_all_categories(user_id):
+    """Retrieve all the user's categories"""
+    sql = '''
+        SELECT category_name 
+        FROM Categories 
+        WHERE user_id = ?
+    '''
+
+    data = (user_id,) 
+    categories_row_obj = query_all(sql, data)
+
+    # Retrieve the category name from each row of the category object
+    categories_list = [category['category_name'] for category in categories_row_obj]
+
+    return categories_list
+
+
+def check_if_add_new_category(category_str, user_id):
+    """Checks whether a user has entered a new category (case sensitive). Returns a boolean."""
+    categories_list = get_all_categories(user_id)
+
+    if category_str not in categories_list:
+        return True
+
+    return False
+
+
 def select_week_dates(selected_date):
     """Returns the start date and end date of the week based on selected date object"""
     # Get the week number of the date
